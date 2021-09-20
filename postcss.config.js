66dd217d01610403ab807da10964a6223f76921d
hrishikesh1990 <hrishikeshpardeshi@gmail.com>
2021-09-20 16:14:12 +0530

    add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,30 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('autoprefixer'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
+let environment = {
+    plugins: [
+        require('postcss-import'),
+        require('autoprefixer'),
+        require('tailwindcss'),
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+            autoprefixer: {
+                flexbox: 'no-2009'
+            },
+            stage: 3
+        })
+    ]
 }
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './app/views/**/*.html.erb',
+          './app/helpers/**/*.rb',
+          './app/javascript/**/*.js',
+          './app/javascript/**/*.jsx',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  )
+}
+
+module.exports = environment
