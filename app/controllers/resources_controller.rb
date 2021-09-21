1636097d3f30140be60bdb94bf8dd5a2608dfb59
hrishikesh1990 <hrishikeshpardeshi@gmail.com>
2021-09-21 13:19:17 +0530

    seo changes

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,6 +5,8 @@ class ResourcesController < ApplicationController
 
   def show
     @resource = Resource.find_by(slug:params[:slug])
+    @title = @resource.title
+    @meta_description = @resource.meta_description
     if @resource.canonical_url.present?
       @canonical_url = @resource.canonical_url
     end
