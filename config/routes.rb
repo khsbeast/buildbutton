9c92e67c2154787be72aea1d041faea8bd37792b
hrishikesh1990 <hrishikeshpardeshi@gmail.com>
2021-10-14 14:28:57 +0530

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'resources#index'
   #get 'resources/index'
   #categories
-  get 'marketing', to:'categories/index'
-  get 'funding', to:'categories/index'
-  get 'product', to:'categories/index'
+  get 'marketing', to:'categories#index'
+  get 'funding', to:'categories#index'
+  get 'product', to:'categories#index'
   get '/marketing/:slug', to:'resources#show'
   get '/funding/:slug', to:'resources#show'
   get '/product/:slug', to:'resources#show'
