d472c020ddbc7fb918758b9ab98951d885cd7292
hrishikesh1990 <hrishikeshpardeshi@gmail.com>
2021-10-14 15:17:59 +0530

    fix routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,21 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def show
+    @category = Category.find_by(slug:params[:category])
+    @resource = Resource.find_by(slug:params[:slug])
+    @core_article = CoreArticle.find_by(slug:params[:slug])
+    @post = @resource ? @resource : @core_article
+    @title = @post.title
+    @meta_description = @post.meta_description
+    if @post.canonical_url.present?
+      @canonical_url = @post.canonical_url
+    end
+    if @resource
+      render 'resources/show'
+    elsif @core_article
+      render 'core_articles/show'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   get 'marketing', to:'categories#index'
   get 'funding', to:'categories#index'
   get 'product', to:'categories#index'
-  get '/marketing/:slug', to:'resources#show'
-  get '/funding/:slug', to:'resources#show'
-  get '/product/:slug', to:'resources#show'
 
+  get '/:category/:slug', to:'categories#show'
   #general blog route
   get ':slug', to:"resources#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
