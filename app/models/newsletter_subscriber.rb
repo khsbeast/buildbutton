62ece61f72c12284fa2732181dd13c3df368347e
Raj Patil <rjpatil53@gmail.com>
2022-01-07 14:42:13 +0530

    Added unique constraint on email address for newsletter subscribers

--- app/models/newsletter_subscriber.rb
+++ app/models/newsletter_subscriber.rb
@@ -1,7 +1,7 @@
 class NewsletterSubscriber < ApplicationRecord
   require 'csv'
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   def self.to_csv
     posts = all
