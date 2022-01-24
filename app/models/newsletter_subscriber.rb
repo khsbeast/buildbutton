8f0672f4cfc85de411cbe992524b740af0571dc0
Sidhant Unnithan <contact@sidhantunnithan.com>
2022-01-24 17:13:30 +0530

    admin to mailchimp newsletter automation

--- app/models/newsletter_subscriber.rb
+++ app/models/newsletter_subscriber.rb
@@ -1,7 +1,11 @@
+require 'httparty'
+
 class NewsletterSubscriber < ApplicationRecord
   require 'csv'
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save :make_api_call
 
   def self.to_csv
     posts = all
@@ -12,4 +16,33 @@ class NewsletterSubscriber < ApplicationRecord
       end
     end
   end
+
+  private
+    def make_api_call
+      chimp_mail_api_key = "0cf5ef71b8e61d200f126c1b73c47bba-us20"
+
+      first_name = name.split[0]
+      last_name = name.split[1]
+
+      if first_name.length == 1
+        first_name = "There"
+      end
+
+      first_name[0] = first_name[0].capitalize
+
+      options = {
+        "email_address" => email,
+        "status"=> "subscribed",
+        "email_type"=> "html",
+        "tags"=> ["New Subscriber"],
+        "merge_fields"=> {
+          "FNAME"=> first_name,
+          "LNAME"=> last_name ? last_name : " "
+        }
+      }
+
+      response = HTTParty.post('https://us20.api.mailchimp.com/3.0/lists/f95aa8bcc2/members', body: options.to_json, headers: {"Authorization" => "Bearer #{chimp_mail_api_key}"})
+      puts response.body
+      
+    end
 end
